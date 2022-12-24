f42f029bbd5315486b75726dd622dae4a9153275
hseniht <thinesh_kumar_mu@hotmail.com>
2022-12-24 13:49:07 +0800

    Main background additions

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,29 @@
 import Box from "@mui/material/Box";
 import Header from "./Header";
 import Toolbar from "@mui/material/Toolbar";
+import bgLeft from "../../assets/bg-Left.svg";
+import bgRight from "../../assets/bg-Right.svg";
+import "./Layout.scss";
+
+const MainBackground = ({ className }) => {
+  return (
+    <div className={className}>
+      <img src={bgLeft} />
+      <img src={bgRight} />
+    </div>
+  );
+};
+
 const Layout = ({ children, ...props }) => {
   return (
     <Box sx={{ display: "flex" }}>
+      <MainBackground className="main-bg" />
       <Header />
-      <Box component="main" sx={{ p: 3, mx: "auto", my: 8 }}>
+      <Box
+        className="app-body"
+        component="main"
+        sx={{ p: 3, mx: "auto", my: 8 }}
+      >
         <Toolbar />
         {children}
       </Box>
