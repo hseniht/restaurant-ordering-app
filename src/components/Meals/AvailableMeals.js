0c6d6d1e47394be59e976f093d0366c8544f95b5
hseniht <thinesh_kumar_mu@hotmail.com>
2022-12-31 14:07:40 +0800

    Use data from firebase instead
    Add fetch functionality to get meals data via HTTP request. Dummy data
    is commented out. Dummy data can be used back in future for fallback value

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,10 +1,42 @@
-import { DUMMY_MEALS } from "./dummy-meals";
+// import { DUMMY_MEALS } from "./dummy-meals";
 import Card from "@mui/material/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./Meals.module.scss";
+import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-480e0-default-rtdb.asia-southeast1.firebasedatabase.app/Meals.json"
+      );
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      //loop object
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      console.log("tk meals", loadedMeals);
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
+  // todo: set fallback to dummy data for DEMO purpose
+  // let mealsData = DUMMY_MEALS;
+  let mealsData = meals;
+
+  const mealsList = mealsData.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
