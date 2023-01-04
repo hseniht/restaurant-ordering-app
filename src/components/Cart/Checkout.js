ae53bb735d56bfea6dcbc2c4b083ae6e5b1dc627
hseniht <thinesh_kumar_mu@hotmail.com>
2023-01-04 23:44:14 +0800

    Add form validation

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,10 +1,50 @@
+import { useRef, useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
+
+//helper
+const isEmpty = (value) => value.trim() === "";
+const isFiveChars = (value) => value.trim().length === 5;
+
 const Checkout = (props) => {
+  const [name, setName] = useState("");
+  const [street, setStreet] = useState("");
+  const [postal, setPostal] = useState("");
+  const [city, setCity] = useState("");
+  const [inputsValidity, setInputsValidity] = useState({
+    name: true,
+    street: true,
+    postal: true,
+    city: true,
+  });
+
   const confirmHandler = (event) => {
     event.preventDefault();
-    console.log("tk submiteed");
+
+    const enteredNameIsValid = !isEmpty(name);
+    const enteredStreetIsValid = !isEmpty(street);
+    const enteredCityIsValid = !isEmpty(city);
+    const enteredPostalIsValid = isFiveChars(postal);
+
+    setInputsValidity({
+      name: enteredNameIsValid,
+      street: enteredStreetIsValid,
+      postal: enteredPostalIsValid,
+      city: enteredCityIsValid,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredCityIsValid &&
+      enteredPostalIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+
+    //submit cart data
   };
   return (
     <form onSubmit={confirmHandler}>
@@ -16,26 +56,52 @@ const Checkout = (props) => {
               label="Your name"
               fullWidth
               variant="standard"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              error={!inputsValidity.name}
+              helperText={!inputsValidity.name && "Please enter a valid name"}
             />
           </Grid>
           <Grid item xs={12}>
             <TextField
               id="street"
               label="Street"
+              value={street}
+              onChange={(e) => setStreet(e.target.value)}
               fullWidth
               variant="standard"
+              error={!inputsValidity.street}
+              helperText={
+                !inputsValidity.street && "Please enter a valid street"
+              }
             />
           </Grid>
           <Grid item xs={6}>
             <TextField
               id="postal"
               label="Postal"
+              value={postal}
+              onChange={(e) => setPostal(e.target.value)}
               fullWidth
               variant="standard"
+              error={!inputsValidity.postal}
+              helperText={
+                !inputsValidity.postal &&
+                "Please enter a valid postcode (5 characters long!)"
+              }
             />
           </Grid>
           <Grid item xs={6}>
-            <TextField id="city" label="City" fullWidth variant="standard" />
+            <TextField
+              id="city"
+              label="City"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              fullWidth
+              variant="standard"
+              error={!inputsValidity.city}
+              helperText={!inputsValidity.city && "Please enter a valid city"}
+            />
           </Grid>
         </Grid>
         <br />
