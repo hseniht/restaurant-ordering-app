d7eb79574da7c304ed6712232b172c6501c98954
hseniht <thinesh_kumar_mu@hotmail.com>
2023-12-27 12:39:12 +0800

    Style cart component

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,4 +1,7 @@
 import classes from "./Cart.module.css";
+import { BtnRound } from "../UI/Buttons";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
 const CartItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
@@ -9,12 +12,31 @@ const CartItem = (props) => {
         <h2>{props.name}</h2>
         <div className={classes.summary}>
           <span className={classes.price}>{price}</span>
-          <span className={classes.amount}>x {props.amount}</span>
+          {/* <span className={classes.amount}>x {props.amount}</span> */}
         </div>
       </div>
       <div className={classes.actions}>
-        <button onClick={props.onRemove}>−</button>
-        <button onClick={props.onAdd}>+</button>
+        <BtnRound
+          size="small"
+          type="submit"
+          aria-label="add"
+          variant="outlined"
+          color="salsa"
+          onClick={props.onRemove}
+        >
+          <RemoveIcon />
+        </BtnRound>
+        <span>{props.amount}</span>
+        <BtnRound
+          size="small"
+          type="submit"
+          aria-label="add"
+          variant="contained"
+          color="salsa"
+          onClick={props.onAdd}
+        >
+          <AddIcon />
+        </BtnRound>
       </div>
     </li>
   );
---
+++ src/components/UI/Buttons.js
@@ -0,0 +1,9 @@
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+
+export const BtnRound = styled(Button)({
+  minWidth: "unset",
+  padding: "2px",
+  margin: "0 12px",
+  borderRadius: "50%",
+});
