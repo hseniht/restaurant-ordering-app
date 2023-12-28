b353666655b6ecdace4c14b2479ea7b8382ff905
hseniht <thinesh_kumar_mu@hotmail.com>
2023-12-28 13:55:37 +0800

    Add modal dialog to show meals detail (WIP)
    Basic functionality like open & close modal with selected item.

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,21 +1,61 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import classes from "../Meals.module.scss";
 import MealItemForm from "./MealItemForm";
 import { CartContext } from "../../../contexts/cart-context";
 import Badge from "@mui/material/Badge";
+import Box from "@mui/material/Box";
+import { MUIDialog } from "../../UI/Modal";
 
-const MealThumbnail = ({ className, image, amount }) => {
+const MealThumbnail = ({ className, image, amount, onClick }) => {
   return (
     <Badge color="salsa" overlap="rectangular" badgeContent={amount}>
-      <img className={className} src={image} />
+      <img className={className} src={image} onClick={onClick} />
     </Badge>
+  );
+};
+
+const MealInfo = ({ name, price, description }) => {
+  return (
+    <div
+      className={classes["meals-info"]}
+      // sx={{ width: "70%", paddingLeft: "1em" }}
+    >
+      <div className={classes["meals-info__heading"]}>
+        <Typography className="test" variant="h6" component="div">
+          {name}
+        </Typography>
+        <Typography
+          component="div"
+          sx={{ fontWeight: "bold" }}
+          color="text.secondary"
+          gutterBottom
+        >
+          {price}
+        </Typography>
+      </div>
+      <Typography className={classes["meals-info__body"]} variant="body2">{description}</Typography>
+    </div>
+  );
+};
+
+const MealDetailedInfo = ({ amount, description, image, name, price }) => {
+  return (
+    <>
+      <MealThumbnail
+        className={classes["menu-thumbnail__dialog"]}
+        image={image}
+        amount={amount}
+      />
+      <MealInfo name={name} price={price} description={description} />
+    </>
   );
 };
 
 const MealItem = ({ id, name, description, price, image, amount }) => {
   const cartCtx = useContext(CartContext);
+  const [openMeal, setOpenMeal] = useState(false);
 
   const prices = `$${price.toFixed(2)}`;
 
@@ -27,6 +67,11 @@ const MealItem = ({ id, name, description, price, image, amount }) => {
       price,
     });
   };
+
+  const handleMenuClick = () => {
+    setOpenMeal(true);
+    console.log("tk open up");
+  };
   return (
     <li className={classes.mealList}>
       {image && (
@@ -34,26 +79,27 @@ const MealItem = ({ id, name, description, price, image, amount }) => {
           className={classes["menu-thumbnail"]}
           image={image}
           amount={amount}
+          onClick={handleMenuClick}
         />
       )}
-      <Stack
-        direction="column"
-        alignItems="flex-start"
-        sx={{ width: "70%", paddingLeft: "1em" }}
+      <MUIDialog
+        // klass={{ root: "test" + classes["menu-dialog__details"] }}
+        rootClass={classes["menu-dialog__details--root"]}
+        dialogClass={classes["menu-dialog__details--body"]}
+        open={openMeal}
+        onClose={setOpenMeal}
+        // title={name}
       >
-        <Typography variant="h6" component="div">
-          {name}
-        </Typography>
-        <Typography
-          component="div"
-          sx={{ fontWeight: "bold" }}
-          color="text.secondary"
-          gutterBottom
-        >
-          {prices}
-        </Typography>
-        <Typography variant="body2">{description}</Typography>
-      </Stack>
+        {/* <MealThumbnail
+          className={classes["menu-thumbnail"]}
+          image={image}
+          amount={amount}
+        />
+        <MealInfo name={name} price={prices} description={description} /> */}
+        {/* Spread Props baby! */}
+        <MealDetailedInfo {...{ amount, description, image, name, price: prices }} />
+      </MUIDialog>
+      <MealInfo name={name} price={prices} description={description} />
       <Stack
         direction="column"
         justifyContent="space-between"
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,6 +1,33 @@
 import { Fragment } from "react";
 import classes from "./Modal.module.css";
 import ReactDOM from "react-dom";
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+
+//Material UI Dialog
+export const MUIDialog = ({
+  rootClass,
+  dialogClass,
+  open,
+  onClose,
+  children,
+  title,
+}) => {
+  const handleClose = () => {
+    onClose(!open);
+  };
+
+  return (
+    <Dialog
+      classes={{ root: rootClass, paper: dialogClass }}
+      onClose={handleClose}
+      open={open}
+    >
+      {title && <DialogTitle>{title}</DialogTitle>}
+      {children}
+    </Dialog>
+  );
+};
 
 const Backdrop = (props) => {
   return <div onClick={props.onClose} className={classes.backdrop}></div>;
